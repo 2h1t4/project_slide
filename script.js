4a85df757892e24655d6a10998b90ed4ee10da3f
2h1t4 <terurute0424@gmail.com>
2026-07-23 13:57:20 +0900

    コミット

---
+++ script.js
@@ -0,0 +1,17 @@
+const game = document.getElementById("game")
+const player = document.getElementById("player");
+const rect = game.getBoundingClientRect()
+console.log("読み込ませる");
+
+document.addEventListener("mousemove",(event)=>{
+
+    let x = event.clientX - rect.left;
+    x -= player.offsetWidth / 2;
+    if(x < 0){
+        x = 0;
+    };
+    if(x > game.clientWidth - player.offsetWidth){
+        x = game.clientWidth - player.offsetWidth
+    }
+    player.style.left = x + "px";
+});
